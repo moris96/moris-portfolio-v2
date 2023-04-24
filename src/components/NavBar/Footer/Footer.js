ec4aa51bdc0af5d8d89167c4fad5d8ac4c7bbce2
moris96 <moriskhoudari@gmail.com>
2023-04-24 14:50:57 -0400

    updated footer with copyright sign

--- src/components/NavBar/Footer/Footer.js
+++ src/components/NavBar/Footer/Footer.js
@@ -6,7 +6,7 @@ export default function Footer(){
     return(
         <>
             <footer>
-                Footer 
+                &copy; Moris Khoudari 2023 
             </footer>
         </>
     );
