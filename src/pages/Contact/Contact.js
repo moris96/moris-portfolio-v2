8be47eebcc7cf211a0770e91fde288724e466672
moris96 <moriskhoudari@gmail.com>
2023-04-24 11:23:50 -0400

    testing new email contact form

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -13,6 +13,16 @@ export default function Contact(){
                 <a href="https://www.linkedin.com/in/moris-khoudari-abady/" target="_blank" className={styles.contactDetails}>Linkedin</a>
                 <a href="https://github.com/moris96" target="_blank" className={styles.contactDetails}>Github</a>
             </div>
+
+            {/* email form */}
+            <form className={styles.OGform} action="mailto:moriskhoudari@gmail.com" method="GET" target="_blank">
+                <h1 className={styles.title}>Start Email to moriskhoudari@gmail.com</h1>
+                <div className={styles.field}><label className={styles.label} for="subject">Email Subject</label>
+                    <input name="subject" id={styles.subject} type="text" className={styles.input} value="Example Email Subject" /></div>
+                <div className={styles.field}><label className={styles.label} for="body">Email Body</label>
+                    <textarea className={styles.textarea} name="body" id={styles.body}>Example Email Body</textarea></div>
+                <div><input type="submit" value="Create Email" className={styles.emailButton} /></div>
+            </form>
         </>
     );
 };
